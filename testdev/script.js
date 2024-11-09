91600272f1469b8d46ca2a778db29c0996d74adf
Raaaphhh <dcpraphael@gmail.com>
2024-11-09 20:46:46 +0100

    diapotets

--- testdev/script.js
+++ testdev/script.js
@@ -4,13 +4,15 @@ let isDragging = false;
 let startPos = 0;
 let currentTranslate = 0;
 let prevTranslate = 0;
+let currentIndex = 0;
 
 diaporama.addEventListener('mousedown', startDrag);
-diaporama.addEventListener('touchstart', startDrag);
+diaporama.addEventListener('touchstart', startDrag, { passive: true });
 diaporama.addEventListener('mouseup', endDrag);
 diaporama.addEventListener('touchend', endDrag);
 diaporama.addEventListener('mousemove', drag);
-diaporama.addEventListener('touchmove', drag);
+diaporama.addEventListener('touchmove', drag, { passive: false });
+diaporama.addEventListener('mouseleave', endDrag);
 
 function startDrag(event) {
     isDragging = true;
@@ -19,19 +21,35 @@ function startDrag(event) {
 }
 
 function endDrag() {
+    if (!isDragging) return;
     isDragging = false;
-    prevTranslate = currentTranslate;
+
+    const movedBy = currentTranslate - prevTranslate;
+    if (movedBy < -100 && currentIndex < diaporama.children.length - 1) currentIndex++;
+    if (movedBy > 100 && currentIndex > 0) currentIndex--;
+
+    setPositionByIndex();
     diaporama.style.cursor = 'grab';
 }
 
 function drag(event) {
     if (!isDragging) return;
+    event.preventDefault();
     const currentPosition = getPositionX(event);
-    const diff = currentPosition - startPos;
-    currentTranslate = prevTranslate + diff;
-    diaporama.style.transform = `translateX(${currentTranslate}px)`;
+    currentTranslate = prevTranslate + currentPosition - startPos;
+    setSliderPosition();
 }
 
 function getPositionX(event) {
     return event.type.includes('mouse') ? event.pageX : event.touches[0].clientX;
+}
+
+function setSliderPosition() {
+    diaporama.style.transform = `translateX(${currentTranslate}px)`;
+}
+
+function setPositionByIndex() {
+    currentTranslate = currentIndex * -diaporama.clientWidth;
+    prevTranslate = currentTranslate;
+    setSliderPosition();
 }
