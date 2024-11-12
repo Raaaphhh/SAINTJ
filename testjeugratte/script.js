1d601783a8157263240f26bb965b02ec83617851
Raaaphhh <dcpraphael@gmail.com>
2024-11-12 16:11:48 +0100

    ticket

--- testjeugratte/script.js
+++ testjeugratte/script.js
@@ -1,3 +1,15 @@
-document.getElementById('bouton-personnalise').addEventListener('click', function() {
+document.getElementById('btn1').addEventListener('click', function() {
+    window.location.href = '../index.html';
+});
+
+document.getElementById('btn2').addEventListener('click', function() {
+    window.location.href = '../index.html';
+});
+
+document.getElementById('btn3').addEventListener('click', function() {
+    window.location.href = '../index.html';
+});
+
+document.getElementById('btn4').addEventListener('click', function() {
     window.location.href = '../index.html';
 });
